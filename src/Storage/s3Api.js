c5feefc411385996c02d673889a4a9d3137ceef7
hiderbrandon <hiderbrandon@gmail.com>
2022-10-28 09:35:03 -0500

    fixed bucket error

--- src/Storage/s3Api.js
+++ src/Storage/s3Api.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
  
-const uploadFile = async (filename, data) => {
-  return s3.putObject({ Bucket: BUCKET, Key: filename, ACL: 'public-read', Body: data });
+const uploadFile = async (filename, data , bucket ) => {
+  return s3.putObject({ bucket: bucket, Key: filename, ACL: 'public-read', Body: data });
 }
 
 module.exports = uploadFile;
--- src/Storage/uploadfileService.js
+++ src/Storage/uploadfileService.js
@@ -6,7 +6,7 @@ const uploadFile = require(`./s3Api`);
 const uploadFileService = async (event)  =>{
     try {
         const { filename, data } = extractFile(event);
-        await uploadFile(filename ,data);
+        await uploadFile(filename ,data , BUCKET);
         myLink = {
             statusCode: 200,
             body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${filename}` })
