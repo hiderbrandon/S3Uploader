d20968216226ecb5a2494063b6b5b7bddd14afe6
hiderbrandon <hiderbrandon@gmail.com>
2022-10-28 04:36:22 -0500

    changed the way to handle promise

--- src/Storage/s3Api.js
+++ src/Storage/s3Api.js
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
  
 const uploadFile = (filename, data) => {
-  return s3.putObject({ Bucket: BUCKET, Key: filename, ACL: 'public-read', Body: data }).promise();
+  return s3.putObject({ Bucket: BUCKET, Key: filename, ACL: 'public-read', Body: data });
 }
 
 module.exports = uploadFile;
