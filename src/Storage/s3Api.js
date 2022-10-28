de1dd9ba2ad2f2140801e9b59a9c096e0456fb2b
hiderbrandon <hiderbrandon@gmail.com>
2022-10-28 04:14:42 -0500

    some logs added

--- src/Storage/s3Api.js
+++ src/Storage/s3Api.js
@@ -6,11 +6,13 @@ const uploadFile = async (filename, data) => {
   try {
     
     await s3.putObject({ Bucket: BUCKET, Key: filename, ACL: 'public-read', Body: data }).promise();
- 
-    return {
+    myLink = {
       statusCode: 200,
       body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${filename}` })
-    }
+    };
+    console.log(`\n============uploadFile==========\n${JSON.stringify(myLink)}\n===========================\n`);
+
+    return myLink;
   } catch (err) {
     return {
       statusCode: 500,
