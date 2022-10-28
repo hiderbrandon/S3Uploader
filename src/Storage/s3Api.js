a0eb9f92dd2710c828f0d041a69add4939f641dd
hiderbrandon <hiderbrandon@gmail.com>
2022-10-28 04:33:58 -0500

    changed the way to handle promise

--- src/Storage/s3Api.js
+++ src/Storage/s3Api.js
@@ -1,24 +1,8 @@
 const AWS = require('aws-sdk');
-const BUCKET = process.env.BUCKET;
 const s3 = new AWS.S3();
  
-const uploadFile = async (filename, data) => {
-  try {
-    
-    await s3.putObject({ Bucket: BUCKET, Key: filename, ACL: 'public-read', Body: data }).promise();
-    myLink = {
-      statusCode: 200,
-      body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${filename}` })
-    };
-    console.log(`\n============S3API==========\n${JSON.stringify(myLink)}\n===========================\n`);
-
-    return myLink;
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: err.stack })
-    }
-  }
+const uploadFile = (filename, data) => {
+  return s3.putObject({ Bucket: BUCKET, Key: filename, ACL: 'public-read', Body: data }).promise();
 }
 
 module.exports = uploadFile;
--- src/Storage/uploadfile.js
+++ src/Storage/uploadfile.js
@@ -1,10 +1,16 @@
-const uploadFile = require(`./s3Api`);
+const BUCKET = process.env.BUCKET;
 const parseMultipart = require('parse-multipart');
+
+const uploadFile = require(`./s3Api`);
 
 const uploadFileService = async (event)  =>{
     try {
         const { filename, data } = extractFile(event);
-        let myLink = await uploadFile(filename ,data);
+        await uploadFile(filename ,data);
+        myLink = {
+            statusCode: 200,
+            body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${filename}` })
+          };
         console.log(`\n============uploadFile==========\n${JSON.stringify(myLink)}\n===========================\n`);
         return myLink;
 
